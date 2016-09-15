65109353c38b990364c4052fee33522258099182
dontworrydawg <bergind777@gmail.com>
2016-09-15 09:08:25 -0400

    Initial Commit

---
+++ mapTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mapTest
+//
+//  Created by Ber Jr on 2016-09-13.
+//  Copyright © 2016 Ber Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
